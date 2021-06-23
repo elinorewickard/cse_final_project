348f465c15ea1490c4f60707c1d0055be5e0484c
elinorewickard <you@example.com>
2021-06-23 10:49:06 -0600

    adding constants to main.py

--- project_template/rename/__main__.py
+++ project_template/rename/__main__.py
@@ -1,1 +1,19 @@
+import arcarde
+
+# Constants for window
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+SCREEN_TITLE = "Vengeance"
+
+# player constants
+GRAVITY = 1
+PLAYER_MOVEMENT_SPEED = 5
+PLAYER_JUMP_SPEED = 20
+
+# margin minimum between character and screen edge
+LEFT_VIEWPORT_MARGIN = 250
+RIGHT_VIEWPORT_MARGIN = 250
+BOTTOM_VIEWPORT_MARGIN = 50
+TOP_VIEWPORT_MARGIN = 100
+
 # program entry point
