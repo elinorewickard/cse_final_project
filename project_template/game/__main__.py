a584b0ac66a065ac58bc141a71a09c7925a1cd2f
elinorewickard <you@example.com>
2021-06-29 20:58:48 -0600

    basic start screen

--- project_template/game/__main__.py
+++ project_template/game/__main__.py
@@ -1,13 +1,16 @@
-
+import constants as c
 import arcade
-from setup import Setup
-
+from startup import Startup
+from instructionView import InstructionView
 # program entry point
 
 def main():
    """ Main method, runs the game when called. """
-   window = Setup()
-   window.startup()
+   window = window = arcade.Window(c.SCREEN_WIDTH, c.SCREEN_HEIGHT, c.SCREEN_TITLE)
+   start_view = InstructionView()
+   game_view = Startup()
+   window.show_view(start_view)
+   game_view.setup()
    arcade.run()
 
 if __name__ == "__main__":
