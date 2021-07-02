9d56e26aa93b950108693309446f3f5c80f4923a
elinorewickard <you@example.com>
2021-07-02 10:25:34 -0600

    Checking

--- project_template/game/instructionView.py
+++ project_template/game/instructionView.py
@@ -16,7 +16,7 @@ class InstructionView(arcade.View):
     def on_draw(self):
         """ Draw this view """
         arcade.start_render()
-        arcade.draw_text("Starting Screen", c.SCREEN_WIDTH / 2, c.SCREEN_HEIGHT / 2,
+        arcade.draw_text("Starting screen", c.SCREEN_WIDTH / 2, c.SCREEN_HEIGHT / 2,
                          arcade.color.WHITE, font_size=50, anchor_x="center")
         arcade.draw_text("Click to continue...", c.SCREEN_WIDTH / 2, c.SCREEN_HEIGHT / 2-75,
                          arcade.color.WHITE, font_size=20, anchor_x="center")
