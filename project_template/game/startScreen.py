bbac276e83ce845b14cb1b3e54c9879deae4488d
elinorewickard <you@example.com>
2021-07-16 11:24:14 -0600

    startscreen and fire speed

--- project_template/game/startScreen.py
+++ project_template/game/startScreen.py
@@ -9,14 +9,14 @@ class StartScreen(arcade.View):
     
     def on_show(self):
       """This runs when we switch to this view."""
-      arcade.set_background_color([34, 139, 34])
+      arcade.set_background_color((200,100,50,100))
       #resets the viewport for scrolling game
       arcade.set_viewport(0, c.SCREEN_WIDTH - 1, 0, c.SCREEN_HEIGHT - 1)
 
     def on_draw(self):
         """ Draw this view """
         arcade.start_render()
-        arcade.draw_text("You've started a fire in the forest and now\nmust collect enough berries to escape!", c.SCREEN_WIDTH / 2, c.SCREEN_HEIGHT / 2,
+        arcade.draw_text("A fire has broken out in the forest and now\nyou must collect enough berries to escape", c.SCREEN_WIDTH / 2, c.SCREEN_HEIGHT / 2,
                          arcade.color.WHITE, font_size=40, anchor_x="center")
         arcade.draw_text("Click to start...", c.SCREEN_WIDTH / 2, c.SCREEN_HEIGHT / 2-75,
                          arcade.color.WHITE, font_size=25, anchor_x="center")
